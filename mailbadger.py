4ca6a96aec0165c4a369e823b3036fd8f682728b
Donald Whyte <donaldwhyte0@gmail.com>
2016-07-06 20:49:21 +0100

    Fixed import errors and made search functionality a subcommand

--- mailbadger.py
+++ mailbadger.py
@@ -2,8 +2,8 @@
 
 """Python script installed with setup.py to run mailbadger script."""
 
-from moduledependency.main import get_argument_parser
-from moduledependency.main import main
+from mailbadger.main import get_argument_parser
+from mailbadger.main import main
 
 if __name__ == '__main__':
     parser = get_argument_parser()
--- mailbadger/main.py
+++ mailbadger/main.py
@@ -1,20 +1,28 @@
 """Contains entrypoint logic for running mailbadger as a script."""
 
-from email_finder.address_validator import AddressValidator
-from email_finder.address_guesser import get_possible_addresses_for
+from mailbadger.address_validator import AddressValidator
+from mailbadger.address_guesser import get_possible_addresses_for
 import argparse
 
 def get_argument_parser():
     parser = argparse.ArgumentParser(
         description='Search email server for possible emails belonging to specified individual')
-    parser.add_argument('first_name', type=str, help='first name of person')
-    parser.add_argument('second_name', type=str, help='second name of person')
-    parser.add_argument('domain', type=str, help='domain of email server to search')
-    parser.add_argument('--verbose', action='store_true', help="enable verbose output")
-    parser.add_argument('--maxNumberToTry', type=int, default=1,
-                        help='max number to append to candidate email')
-    parser.add_argument('--numProcesses', type=int, default=4,
-                        help='number of processes to spawn for validating emails in parallel')
+    subparsers = parser.add_subparsers()
+
+    search_parser = subparsers.add_parser('search',
+        help='search for a person\'s email address on a specific email domain')
+    search_parser.add_argument('first_name', type=str,
+        help='first name of person')
+    search_parser.add_argument('second_name', type=str,
+        help='second name of person')
+    search_parser.add_argument('domain', type=str,
+        help='domain of email server to search')
+    search_parser.add_argument('--verbose', action='store_true',
+        help="enable verbose output")
+    search_parser.add_argument('--maxNumberToTry', type=int, default=1,
+        help='max number to append to candidate email')
+    search_parser.add_argument('--numProcesses', type=int, default=4,
+        help='number of processes to spawn for validating emails in parallel')
 
     return parser
 
